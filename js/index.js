e57e9710ceba795ea1ed454e9d367774c1f60631
woobin huh <madgb89@naver.com>
2018-07-04 18:10:12 +0900

    alerts

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$(".left-menus p").click(function(event) {
+		alert("sorry it's blocked for experimental reason.")
+	});
+	$(".left-menus button").click(function(event) {
+		alert("sorry it's blocked for experimental reason.")
+	});
+	$(".page-title-left").click(function(event) {
+		alert("sorry it's blocked for experimental reason.")
+	});
+	$(".login-button").click(function(event) {
+		alert("sorry it's blocked for experimental reason.")
+	});	
+});
